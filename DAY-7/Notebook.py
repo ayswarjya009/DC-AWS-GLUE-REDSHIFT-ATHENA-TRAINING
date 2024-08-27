c0dd47548d309dc8668e26031678c3c63e945448
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-08-27 17:12:13 +0530

    Update Notebook.py

--- DAY-7/Notebook.py
+++ DAY-7/Notebook.py
@@ -8,3 +8,7 @@ spark = SparkSession.builder.appName("AWS Glue Notebook").getOrCreate()
 s3_bucket = "my-glue-bucket"
 file_path = "s3://{}/your-data-file.csv".format(s3_bucket)
 df = spark.read.csv(file_path, header=True, inferSchema=True)
+
+
+
+transformed_df = df.filter(df["some_column"] > 100).select("column1", "column2")
