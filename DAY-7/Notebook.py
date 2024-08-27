669b6abca27927407b7ff0f873688762ed1ea4d3
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-08-27 17:15:06 +0530

    Update Notebook.py

--- DAY-7/Notebook.py
+++ DAY-7/Notebook.py
@@ -12,3 +12,9 @@ df = spark.read.csv(file_path, header=True, inferSchema=True)
 
 
 transformed_df = df.filter(df["customer_id"] > 1002).select("customer_id", "customer_name")
+
+
+
+output_path = "s3://{}/transformed-data/".format(s3_bucket)
+transformed_df.write.mode("overwrite").csv(output_path, header=True)
+
