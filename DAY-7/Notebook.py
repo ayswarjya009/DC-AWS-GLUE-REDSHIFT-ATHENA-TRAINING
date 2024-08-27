194fe0ce069cf66e22a2bd84ebab9f617804edb6
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-08-27 17:08:21 +0530

    Update Notebook.py

--- DAY-7/Notebook.py
+++ DAY-7/Notebook.py
@@ -3,3 +3,8 @@ import pandas as pd
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("AWS Glue Notebook").getOrCreate()
+
+
+s3_bucket = "my-glue-bucket"
+file_path = "s3://{}/your-data-file.csv".format(s3_bucket)
+df = spark.read.csv(file_path, header=True, inferSchema=True)
