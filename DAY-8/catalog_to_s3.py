6aa92aa1dda48b6c991f00ba21cdb31f420ad4ea
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-08-28 16:15:33 +0530

    Update catalog_to_s3.py

--- DAY-8/catalog_to_s3.py
+++ DAY-8/catalog_to_s3.py
@@ -10,6 +10,24 @@ glueContext = GlueContext(SparkContext.getOrCreate())
 spark = glueContext.spark_session
 
 
+database_name = "hks_sample_db"
+table_name = "customers"
+
+
+dynamic_frame= glueContext.create_dynamic_frame.from_catalog(database = database_name, table_name = table_name)
+
+dynamic_frame.toDF().show(5)
+
+output_path = "s3://hks-demo/output-new-catalog-s3/"
+
+glueContext.write_dynamic_frame.from_options(
+    frame = dynamic_frame, 
+    connection_type = "s3", 
+    connection_options = {"path":output_path}, format = "csv",
+    format_options={"seperator": ","}
+)
+
+
 
 
 
