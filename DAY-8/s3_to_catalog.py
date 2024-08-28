e99f5034d8f11570305de524ab999fd83fd98b7f
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-08-28 14:14:48 +0530

    Update s3_to_catalog.py

--- DAY-8/s3_to_catalog.py
+++ DAY-8/s3_to_catalog.py
@@ -2,3 +2,19 @@ import boto3
 glue_client = boto3.client('glue')
 
 
+database_name = "hks_demo_test"
+response = glue_client.create_database(    
+    DatabaseInput={
+        'Name': database_name,
+        'Description': 'This is my test DB'
+    }
+)
+print("Database Created: ", response)
+
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+from awsglue.dynamicframe import DynamicFrame
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+
